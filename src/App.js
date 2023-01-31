a03c26c5d24f4f1854ac077d48d41964c71f1dbe
Keyur Italiya <italiyakeyur@gmail.com>
2023-01-31 15:55:10 +0530

    commit- local storage

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useRef } from "react";
 
 function App() {
+
+  const data=useRef();
+    const handleClick=()=>{
+        console.log(data.current.value,"initial value")
+        localStorage.setItem("inputValue",data.current.value)
+    }
+    console.log(localStorage.getItem("inputValue"),"****")
+
   return (
+    <>
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <input ref={data} />
+        <button onClick={handleClick}>Add</button>
       </header>
     </div>
+    </>
   );
 }
 
